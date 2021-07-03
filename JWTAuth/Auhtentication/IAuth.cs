c293072b05f0751245286a762c7a3e9e473eca37
marwanmar123 <youcode098@gmail.com>
2021-07-03 16:02:51 +0100

    Add project files.

---
+++ JWTAuth/Auhtentication/IAuth.cs
@@ -0,0 +1,16 @@
+﻿using JWTAuth.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JWTAuth.Auhtentication
+{
+    public interface IAuth
+    {
+        Task<TokenAuth> Register(Register register);
+        Task<TokenAuth> DoLogin(Login token);
+        Task<string> AddRole(RoleWork role);
+        Task<string> CancelRole(RoleWork role);
+    }
+}
---
+++ JWTAuth/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using JWTAuth.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JWTAuth.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<User>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
